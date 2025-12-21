6f39e00ef424d0b0aa4eabea3b7e04d13ddff747
Ilya Gurpinar <ilyagurpinar@gmail.com>
2025-12-21 18:20:41 +0300

    Initial minishell skeleton

---
+++ minishell.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void msh_loop();
+
+int main(int argc, char **argv) {
+    
+    msh_loop();
+    
+    return EXIT_SUCCESS;
+}
+
+void msh_loop(){
+    printf("Hello\n");    
+}
